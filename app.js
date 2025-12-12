53b5eb3a663cee729b5dd3aac7cbe7420607acd5
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2025-12-12 10:45:28 +0530

    Seventh commit

--- app.js
+++ app.js
@@ -1,119 +1,109 @@
 const product=[
     {
         id: 1,
-        image:"C:image\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\bacon-burger-beef.jpg",
+        // image:'asset/asset/bacon-burger-beef.jpg',
         Name:"Bacon Burger",
         price: 1750.00
 
     },
      {
         id:2,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\bacon-cheddar-jalapeno-burger.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\bacon-cheddar-jalapeno-burger.jpg",
         name:"bacon-cheddar-jalapeno-burger",               
         price: 2000.00
     },
     {
           id:3,
-        image:"",
+        // image:"",
         name:"chili Crunch Burger",              
         price: 1100.00
     },
     {
          id:3,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\classic.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\classic.jpg",
         name:"Classic Burger",              
         price: 430.00
 },
+
 {
     id:4,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\classic.jpg",
-        name:"Classic Burger",              
-        price: 430.00
-},
-{
-    id:5,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\double-stack-beef-burger.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\double-stack-beef-burger.jpg",
         name:"Double stack beef Burger",              
         price: 2600.00
 },
 {
     id:6,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\hawaiian-burger-pineapple-ham-cheese-topped-crispy-fried-onion.jpg",
+    //   image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\hawaiian-burger-pineapple-ham-cheese-topped-crispy-fried-onion.jpg",
         name:"Hwaiian Burger",              
         price: 1900.00
 },
 
 {
     id:7,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\mushroom-swiss-burger.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\mushroom-swiss-burger.jpg",
         name:"Mushroom Burger",                            
         price: 1430.00
 },
 
-{
-    id:8,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\mushroom-swiss-burger.jpg",
-        name:"Truffle Burger",                                           
-        price: 1000.00
-},
+
  {
     id:1,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Classic French Fries.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Classic French Fries.jpg",
         name:"Classic French Fries",                                        
         price:550.00
 },
   {
     id:2,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Curly Fries.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Curly Fries.jpg",
         name:"Curly Fries",
         price:626.00                                        
 },
    {
     id:3,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Sweet Potato Fries.jpg,",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Sweet Potato Fries.jpg,",
         name:"Sweet Potato Fries",
         price:1645.00                                      
 },
     {
     id:4,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Waffle Fries.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Waffle Fries.jpg",
         name:"Waffle Fries",
         price:1000.00                                     
 },
     {
     id:1,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\coca.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\coca.jpg",
         name:"Coca-cola",
         price:180.00                                     
 },
    {
     id:2,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantaorange.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantaorange.jpg",
         name:"Fanta Orange",
         price:150.00                                     
 },
    {
     id:3,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantapinaple.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantapinaple.jpg",
         name:"Fanta Pinaple",
         price:200.00                                    
 },
 {
     id:4,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantapinaple.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantapinaple.jpg",
         name:"Fanta Pinaple",
         price:200.00                                    
 },
   
 {
     id:5,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\pepsi.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\pepsi.jpg",
         name:"Pepsi",
         price:180.00                                    
 },
 {
     id:6,
-        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Cream-soda.jpg",
+        // image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Cream-soda.jpg",
         name:"Cream-Soda",
         price:180.00                                    
 },
@@ -142,9 +132,35 @@ document.getElementById('root').innerHTML=itemsCategories.map((item)=>
 
 var cart=[];
 
+function addorder(a){
+    cart.push({... itemsCategories(a)});
+    display();
+}
+
+function delElement(a){
+    cart.splice(a, 1);
+    display();
+}
+
 function display(a){
     let i=0;
+    document.getElementById("count").innerHTML=cart.length;
     if(cart.length==0){
-        document.getElementById('product')
+        document.getElementById('product').innerHTML="Your cart is empty";
     }
-}
+    else{
+        document.getElementById("product").innerHTML=cart.map((items)=>
+        {
+            var{image,name,price}=items;
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                    </div>
+                    <p style='font-size:20px;'>${name}</p>
+                    <h2 style='font-size:23px;'>${price}.00</h2>`+
+                " <i class = 'fa-soild fa-trash'onclic='delElement("+(i++)+")'></i></div>"
+            );
+    }).join('');
+    }
+}z
